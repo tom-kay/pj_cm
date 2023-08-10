6163ebcd3e6f52b62ef90eb0c98449330761933e
tom-kay <bgfriend83@gmail.com>
2023-08-10 21:31:03 +0900

    banner xxxx

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,27 +1,39 @@
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import Carousel from "react-bootstrap/Carousel";
+import "./Banner.css";
 
 function Banner() {
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <ExampleCarouselImage text="First slide" />
+    <Carousel className="Banner" data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="../img/banner/1225123.jpg/800x400?text=First slide&bg=f5f5f5"
+          alt="First slide"
+        />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <ExampleCarouselImage text="Second slide" />
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=eee"
+          alt="Second slide"
+        />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+          alt="Third slide"
+        />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h5>Third slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
@@ -30,4 +42,55 @@ function Banner() {
     </Carousel>
   );
 }
+
 export default Banner;
+
+// import "./Banner.css";
+// import Carousel from "react-bootstrap/Carousel";
+// // import ExampleCarouselImage from "../img/banner";
+
+// function Banner() {
+//   return (
+//     // <div>
+//     //   <div
+//     //     id="carouselExampleSlidesOnly"
+//     //     class="carousel slide"
+//     //     data-bs-ride="carousel"
+//     //   >
+//     //     <div class="carousel-inner">
+//     //       <div class="carousel-item active">
+//     //         <img
+//     //           src="../img/banner/1225123.jpg"
+//     //           class="d-block w-100"
+//     //           alt="..."
+//     //         />
+//     //       </div>
+//     //       <div class="carousel-item">
+//     //         <img src="..." class="d-block w-100" alt="..." />
+//     //       </div>
+//     //       <div class="carousel-item">
+//     //         <img src="..." class="d-block w-100" alt="..." />
+//     //       </div>
+//     //     </div>
+//     //   </div>
+//     // </div>
+//     <div className="Banner">
+//       <Carousel className="bannerCarousel" data-bs-theme="dark">
+//         <Carousel.Item>
+//           <img
+//             className="d-block w-100"
+//             src="../img/banner/1225123.jpg"
+//             alt="1225123"
+//           />
+
+//           {/* <ExampleCarouselImage text="1225123" /> */}
+//           <Carousel.Caption>
+//             <h5>First slide label</h5>
+//             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//           </Carousel.Caption>
+//         </Carousel.Item>
+//       </Carousel>
+//     </div>
+//   );
+// }
+// export default Banner;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,31 +1,31 @@
 // 메뉴바 만들기
-import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
+import { Nav, NavDropdown, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
-function Header(){
+function Header() {
   return (
     <div className="Header">
-      <Navbar fixed="top" className="bg-body-tertiary justify-content-between">
+      {/* fixed="top"  */}
+      <Navbar className="bg-body-tertiary justify-content-between">
         <Container>
           <Navbar.Brand href="/">LOGO</Navbar.Brand>
           {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-            <Nav className="me-auto">
-              <NavDropdown title="박물관" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/area">지역별</NavDropdown.Item>
-                <NavDropdown.Item href="/subject">주제별</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/mark">북마크</Nav.Link>
-              <Nav.Link href="/review">후기</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="/mark">로그인</Nav.Link>
-              <Nav.Link href="/mark">회원가입</Nav.Link>
-            </Nav>
+          <Nav className="me-auto">
+            <NavDropdown title="박물관" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/area">지역별</NavDropdown.Item>
+              <NavDropdown.Item href="/subject">주제별</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/mark">북마크</Nav.Link>
+            <Nav.Link href="/review">후기</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/mark">로그인</Nav.Link>
+            <Nav.Link href="/mark">회원가입</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-
-    </div>  
-  )
+    </div>
+  );
 }
 
 export default Header;
